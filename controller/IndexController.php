4136bab0e82a763b35eb90bf252b1a230d97422d
Yan Burtovoy <mig291988@gmail.com>
2014-01-28 13:01:24 +0400

    dir structure

---
+++ controller/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ya
+ * Date: 1/28/14
+ * Time: 12:52 PM
+ */
+
+namespace controller;
+
+
+class IndexController {
+
+} 
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ya
+ * Date: 1/28/14
+ * Time: 12:48 PM
+ */ 
